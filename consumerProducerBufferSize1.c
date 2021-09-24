9b4cf371ba00ee824dcdce05378dd274f999dc7a
Giovana Queiroz <giovana.oliveira@ccc.ufcg.edu.br>
2021-09-24 17:03:16 -0300

    prodConsBuffer1

---
+++ consumerProducerBufferSize1.c
@@ -0,0 +1,38 @@
+int buffer;
+long counter = 0; //protocols starts with empty buffer
+
+void put(int value)
+{
+    assert(counter == 0);
+    counter = 1;
+    buffer = value;
+}
+
+int get()
+{
+    assert(counter == 1);
+    counter = 0;
+    return buffer;
+}
+
+void *produce(void *args)
+{
+    int i;
+    int items_to_produce = (int)args;
+    for (i = 0; i < items_to_produce; i++)
+    {
+        put(i);
+    }
+    return NULL;
+}
+
+void *consume(void *args)
+{
+    int i;
+    while (1)
+    {
+        int tmp = get();
+        prinf("%d\n", tmp);
+    }
+    return NULL;
+}
