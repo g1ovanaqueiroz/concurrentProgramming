c5f4e44c9d885ddf5125775c159125ba1e5cb8c4
Giovana Queiroz <giovana.oliveira@ccc.ufcg.edu.br>
2021-09-25 20:26:10 -0300

    fibonacci update

--- go/fibonacci.go
+++ go/fibonacci.go
@@ -2,17 +2,27 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	n := 42
+	go alert()
 	result := fib(n)
-	fmt.Printf("fib(%d = %d\n", n, result)
+	fmt.Printf("Fib(%d) is %d\n", n, result)
+
+}
+
+func alert() {
+	for {
+		time.Sleep(1 * time.Second)
+		fmt.Printf("It's not over yet, wait!\n")
+	}
 }
 
 func fib(x int) int {
 	if x < 2 {
-		return x
+		return 2
 	}
 	return fib(x-1) + fib(x-2)
 }
